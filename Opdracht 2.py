4ee58ee74691f45981ddc98a810c2cd7811dd437
Tjees <tjez.vantuijl@student.hu.nl>
2023-02-08 16:41:01 +0100

    Extra Toegevoegd.

--- Opdracht 2.py
+++ Opdracht 2.py
@@ -7,6 +7,15 @@ from skimage import color
 from skimage import data, filters
 from scipy import ndimage
 
+# Aparte imports voor functies weggehaald want is niet echt nodig.
+
+def makeHueArray( image ):
+    hueArray = []
+    for i in image:
+        for j in i:
+            hueArray.append( j[0] * 360 ) # Hue value is 0 - 1 dus keer 360 want dan krijg je de hue.
+    return hueArray
+
 image = io.imread('C:/Users/tjezv/OneDrive/Afbeeldingen/London.jpg') # RGB test afbeelding of image.
 print( image )
 
@@ -22,5 +31,8 @@ fig, axs = plt.subplots( 2, 3, figsize=(8,4) )
 
 axs[0, 0].imshow( image )
 axs[0, 1].imshow( color.gray2rgb( newimage ) ) # Terug naar rgb image anders werkt het displayen niet.
+
+axs[1, 0].hist( makeHueArray( color.rgb2hsv( image ) ) )
+axs[1, 1].hist( makeHueArray( color.rgb2hsv( color.gray2rgb( newimage ) ) ) )
 
 plt.show()
