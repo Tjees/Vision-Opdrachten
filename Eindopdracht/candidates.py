3044526a44f86f3a6b9372f75211b741f1424f42
Tjees <tjez.vantuijl@student.hu.nl>
2023-03-17 01:42:13 +0100

    A.

--- Eindopdracht/candidates.py
+++ Eindopdracht/candidates.py
@@ -15,3 +15,18 @@ def randomCandidate( img, size ):
 def testCandidate( img ):
     img = img[270:335, 230:415]
     return img, (230, 415, 270, 335)
+
+def sweepCandidates( img, size ):
+    candidates = []
+    bboxes = []
+    posX = 0
+    posY = 0
+    while posY < ( img.shape[0] - size[1] ):
+        while posX < ( img.shape[1] - size[0] ):
+            candidates.append(img[posY:posY+size[1], posX:posX+size[0]])
+            bboxes.append((posX, posX+size[0], posY, posY+size[1]))
+            posX += size[0]//4
+        posY += size[1]//4
+        posX = 0
+    
+    return np.array(candidates), np.array(bboxes)
--- Eindopdracht/main.py
+++ Eindopdracht/main.py
@@ -27,35 +27,37 @@ from keras.models import model_from_json
 #     image = cutOutRandom( image, (224, 224), xMin[i], xMax[i], yMin[i], yMax[i] )
 #     saveCroppedImage( image, 'C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Eindopdracht/dataset/No Plates/', 'no_plate_' + loadData()[0][i] + '.jpg') # Image naam moet zelfde naar als originele image bevatten, anders kan bijvoorbeeld plate0 de kentekenplaat van img_2297 bevatten.
 
-# json_file = open('C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Eindopdracht/my_model_arch.json', 'r')
-# classifier = model_from_json(json_file.read(), custom_objects={'KerasLayer':hub.KerasLayer})
-# classifier.load_weights('C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Eindopdracht/my_model_weights.h5')
+json_file = open('C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Eindopdracht/my_model_arch.json', 'r')
+classifier = model_from_json(json_file.read(), custom_objects={'KerasLayer':hub.KerasLayer})
+classifier.load_weights('C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Eindopdracht/my_model_weights.h5')
 
-# classifier.compile(
-#     optimizer="adam",
-#     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#     metrics=['acc'])
+classifier.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['acc'])
 
-# # classifier = trainModel()
+# classifier = trainModel()
 
-# license_plate = np.array(Image.open('C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Transfer Test/Car-Number-Plate.jpg'))
-# license_plate_candidate, bbox = testCandidate( np.array(license_plate) )
-# license_plate_candidate = convertBBoxImage( license_plate_candidate, (224, 224) )
-# print(license_plate_candidate)
+license_plate = np.array(Image.open('C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Transfer Test/car.jpg'))
+license_plate_candidates, bboxes = sweepCandidates( np.array(license_plate), (320, 240))
 
-# license_plate_candidate = np.array(license_plate_candidate)/255.0
-# print(license_plate_candidate.shape)
-# print((license_plate_candidate[np.newaxis, ...]).shape)
+for i in range(len(license_plate_candidates)-1):
+    print(license_plate_candidates[i].shape)
+    license_plate_candidate = convertBBoxImage( license_plate_candidates[i], (224, 224) )
+    license_plate_candidate = np.array(license_plate_candidate)/255.0
+    print(license_plate_candidate.shape)
+    if classifier.predict(license_plate_candidate[np.newaxis, ...]) > 0.85:
+        cv.rectangle( license_plate, (bboxes[i][0], bboxes[i][2]), (bboxes[i][1], bboxes[i][3]), (255,0,0), 3 )
+        # break
 
 # result = classifier.predict(license_plate_candidate[np.newaxis, ...])
-
 # if result[0] > 0.7:
 #     cv.rectangle( license_plate, (bbox[0], bbox[2]), (bbox[1], bbox[3]), (255,0,0), 3 )
 
 # print(result)
 # print(result.shape)
 
-# classifier.summary()
+classifier.summary()
 
 # file, xMin, xMax, yMin, yMax, label = loadData()
 # image = np.array(Image.open("C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Eindopdracht/images/" + str(file[0]) + ".jpg"))
@@ -64,7 +66,5 @@ from keras.models import model_from_json
 # print(image.shape)
 # plt.imshow(image)
 # plt.show()
-image = convertBBoxImage(cutOutBBox(0), (640,480))
-print(image.shape)
-plt.imshow(image)
+plt.imshow(license_plate)
 plt.show()
