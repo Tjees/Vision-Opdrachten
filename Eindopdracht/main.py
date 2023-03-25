810d0bafb8c400504a4f57d74ba58f8f873213f3
Tjees <tjez.vantuijl@student.hu.nl>
2023-03-25 03:51:41 +0100

    A.

--- Eindopdracht/main.py
+++ Eindopdracht/main.py
@@ -13,6 +13,62 @@ from keras.models import Sequential
 from keras.models import load_model
 from keras.models import model_from_json
 
+def compute_overlap(box1, box2):
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
+
+    intersection_area = max(0, x2 - x1 + 1) * max(0, y2 - y1 + 1)
+
+    box1_area = (box1[2] - box1[0] + 1) * (box1[3] - box1[1] + 1)
+    box2_area = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)
+
+    x = float(box1_area + box2_area - intersection_area)
+
+    if x > 0: 
+        overlap_ratio = intersection_area / x
+    else:
+        overlap_ratio = 0
+
+    return overlap_ratio
+
+def non_max_suppression(candidates, bboxes, overlap_thres):
+    areas = []
+    for bbox in bboxes:
+        areas.append((bbox[2]-bbox[0]) * (bbox[3]-bbox[1]))
+
+    areas = np.array(areas)
+
+    sorted_indices = np.argsort(-areas)
+
+    sorted_bboxes = bboxes[sorted_indices]
+    sorted_candidates = candidates[sorted_indices]
+
+    sorted_bboxes_copy = np.copy(sorted_bboxes)
+    sorted_candidates_copy = np.copy(sorted_candidates)
+
+    picked_bboxes = []
+    picked_candidates = []
+
+    num_boxes = len(bboxes)
+
+    for i in range(num_boxes):
+        picked_bboxes.append(sorted_bboxes[i])
+        picked_candidates.append(sorted_candidates[i])
+
+        overlapping_boxes = []
+        for j in range(i+1, num_boxes):
+            if compute_overlap(sorted_bboxes_copy[i], sorted_bboxes_copy[j]) > overlap_thres:
+                overlapping_boxes.append(j)
+
+        sorted_bboxes_copy = np.delete(sorted_bboxes_copy, overlapping_boxes, axis=0)
+        sorted_candidates_copy = np.delete(sorted_candidates_copy, overlapping_boxes, axis=0)
+
+        num_boxes -= len(overlapping_boxes)
+
+    return np.array(picked_candidates), np.array(picked_bboxes)
+
 # Notes:
 # Bij de predict, i.p.v. de index van de label op te zoeken op basis van de hoogste waarde ( meerdere classes ), dus softmax,
 # wordt nu sigmoid gebruikt. Als de waarde dan dichter bij de label is is het dus wel een kentekenplaat en als die bij de 1 ligt niet.
@@ -38,69 +94,88 @@ classifier.compile(
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=['acc'])
 
-license_plate = cv.imread('C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Transfer Test/car9.jpg')
-license_plate = cv.cvtColor(license_plate, cv.COLOR_BGR2RGB)
+vid = cv.VideoCapture(0)
 
-# license_plate = cv.resize(license_plate, (1920,1080), cv.INTER_AREA)
+while(True):
 
-license_plate_candidates, bboxes = createCandidatePositions(license_plate)
+    ret, license_plate = vid.read()
 
-# license_plate_candidates, bboxes = sweepCandidates(createClustering(license_plate))
+    # license_plate = cv.imread('C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Transfer Test/car3.jpg')
+    # license_plate = cv.cvtColor(license_plate, cv.COLOR_BGR2RGB)
 
-# for i in range(len(license_plate_candidates)-1):
-#     print( license_plate_candidates[i].shape )
-#     print( bboxes[i] )
-#     cv.rectangle( license_plate, (bboxes[i][0], bboxes[i][2]), (bboxes[i][1], bboxes[i][3]), (255,0,0), 3 )
+    # license_plate = cv.resize(license_plate, (1920,1080), cv.INTER_AREA)
 
-print(license_plate_candidates[0].shape)
+    license_plate_candidates, bboxes = createCandidatePositions(license_plate)
 
-convertedCandidates = []
-for i in range(len(license_plate_candidates)-1):
-    convertedCandidates.append(convertBBoxImage(license_plate_candidates[i], (224,224)))
+    license_plate_candidates_NMS, bboxes_NMS = non_max_suppression(license_plate_candidates,bboxes,0.75)
 
-license_plate_candidates = np.array(convertedCandidates)/255.0
+    print(license_plate_candidates_NMS.shape)
 
-print(license_plate_candidates.shape)
+    # license_plate_candidates, bboxes = sweepCandidates(createClustering(license_plate))
 
-results = classifier.predict(license_plate_candidates)
+    # for i in range(len(license_plate_candidates)-1):
+    #     print( license_plate_candidates[i].shape )
+    #     print( bboxes[i] )
+    #     cv.rectangle( license_plate, (bboxes[i][0], bboxes[i][2]), (bboxes[i][1], bboxes[i][3]), (255,0,0), 3 )
 
-print(results)
+    # print(license_plate_candidates[0].shape)
 
-# results = []
-# bboxesResults = []
-# for i in range(len(license_plate_candidates)-1):
-#     print(license_plate_candidates[i].shape)
-#     license_plate_candidate = convertBBoxImage( license_plate_candidates[i], (224, 224) )
-#     license_plate_candidate = np.array(license_plate_candidate)/255.0
-#     print(license_plate_candidate.shape)
-#     results.append( classifier.predict(license_plate_candidate[np.newaxis, ...])[0] )
-#     bboxesResults.append( bboxes[i] )
+    convertedCandidates = []
+    for i in range(len(license_plate_candidates_NMS)-1):
+        convertedCandidates.append(convertBBoxImage(license_plate_candidates_NMS[i], (224,224)))
 
-maxIndex = np.where(results == np.max(results))[0][0]
-print(maxIndex)
-print(results[maxIndex])
-if results[maxIndex] > 0.5:
-    cv.rectangle( license_plate, (bboxes[maxIndex][0], bboxes[maxIndex][2]), (bboxes[maxIndex][1], bboxes[maxIndex][3]), (255,0,0), 3 )
-# break
+    license_plate_candidates_NMS = np.array(convertedCandidates)/255.0
 
-# for i in range(len(results)):
-#     if results[i][0] > 0.6:
-#         cv.rectangle( license_plate, (bboxes[i][0], bboxes[i][2]), (bboxes[i][1], bboxes[i][3]), (255,0,0), 3 )
-#         print(results[i][0])
+    # print(license_plate_candidates.shape)
 
-# result = classifier.predict(license_plate_candidate[np.newaxis, ...])
+    results = classifier.predict(license_plate_candidates_NMS)
 
-# print(result)
-# print(result.shape)
+    # print(results)
 
-classifier.summary()
+    # results = []
+    # bboxesResults = []
+    # for i in range(len(license_plate_candidates)-1):
+    #     print(license_plate_candidates[i].shape)
+    #     license_plate_candidate = convertBBoxImage( license_plate_candidates[i], (224, 224) )
+    #     license_plate_candidate = np.array(license_plate_candidate)/255.0
+    #     print(license_plate_candidate.shape)
+    #     results.append( classifier.predict(license_plate_candidate[np.newaxis, ...])[0] )
+    #     bboxesResults.append( bboxes[i] )
 
-# file, xMin, xMax, yMin, yMax, label = loadData()
-# image = np.array(Image.open("C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Eindopdracht/images/" + str(file[0]) + ".jpg"))
+    maxIndex = np.where(results == np.max(results))[0][0]
+    print(maxIndex)
+    print(results[maxIndex])
+    if results[maxIndex] > 0.6:
+        cv.rectangle( license_plate, (bboxes_NMS[maxIndex][0], bboxes_NMS[maxIndex][2]), (bboxes_NMS[maxIndex][1], bboxes_NMS[maxIndex][3]), (255,0,0), 3 )
+    # break
 
-# image = cutOutRandom( image, (224, 224), xMin[0], xMax[0], yMin[0], yMax[0] )
-# print(image.shape)
-# plt.imshow(image)
-# plt.show()
-plt.imshow(license_plate)
-plt.show()
+    # for i in range(len(results)):
+    #     if results[i][0] > 0.6:
+    #         cv.rectangle( license_plate, (bboxes[i][0], bboxes[i][2]), (bboxes[i][1], bboxes[i][3]), (255,0,0), 3 )
+    #         print(results[i][0])
+
+    # result = classifier.predict(license_plate_candidate[np.newaxis, ...])
+
+    # print(result)
+    # print(result.shape)
+
+    # classifier.summary()
+
+    # file, xMin, xMax, yMin, yMax, label = loadData()
+    # image = np.array(Image.open("C:/Users/tjezv/OneDrive/Desktop/Vision Opdrachten/Eindopdracht/images/" + str(file[0]) + ".jpg"))
+
+    # image = cutOutRandom( image, (224, 224), xMin[0], xMax[0], yMin[0], yMax[0] )
+    # print(image.shape)
+    # plt.imshow(image)
+    # plt.show()
+    # plt.imshow(license_plate)
+    # plt.show()
+    cv.imshow('frame', license_plate)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv.destroyAllWindows()
